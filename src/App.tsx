2a3a166f73af1db04740263558dcd0a4fe4201e0
Karthik-guddanti <karthikguddanti2503@gmail.com>
2025-08-25 10:39:00 +0530

    .

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import LandingPage from './pages/LandingPage';
-import HostelListPage from './pages/HostelListPage';
+import HostelListPage from "./pages/HostelListPage/HostelListPage";
+
 
 const App: React.FC = () => {
     return (
         <BrowserRouter>
             <Routes>
-                <Route path="/" element={<LandingPage />} />
+                
                 <Route path="/hostels" element={<HostelListPage />} />
                 {/* Add more routes here as your project grows */}
             </Routes>
